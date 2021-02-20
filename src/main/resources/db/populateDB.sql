260575bf8e04e53fa18fae3274000a987e4cd47b
Alexander Babich <albabich@gmail.com>
2021-02-21 01:10:00 +0300

    Do HW03

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,9 @@
-DELETE FROM user_roles;
-DELETE FROM users;
+DELETE
+FROM meals;
+DELETE
+FROM user_roles;
+DELETE
+FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -9,3 +13,10 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, datetime, description, calories)
+VALUES ('100000', '2020-01-30 10:00:00', 'Завтрак', '500'),
+       ('100000', '2020-01-30 14:00:00', 'Обед', '1000'),
+       ('100001', '2020-01-30 13:00:00', 'Обед', '1200'),
+       ('100000', '2020-01-30 20:00:00', 'Ужин', '500');
+
